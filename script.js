a406e0dafdc7fc33b79d2098f7b781561370eae5
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-23 19:21:18 +0800

    Add header

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function book(author, title, numOfPages, isRead) {
+    this.author = author;
+    this.title = title;
+    this.numOfPages = numOfPages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+    
+}
