fd91108783f3dca7c08625cf68ebccbbec625b53
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-25 00:00:33 +0800

    Update JS script, Add functionality to buttons

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const myLibrary = [];
 
-const addButton = document.querySelector('.add-button');
-const closeButton = document.querySelector('.cancel-button');
+const openFormButton = document.querySelector('.open-form-button');
+const closeFormButton = document.querySelector('.cancel-button');
+const addBookButton = document.querySelector('.add-button')
 const dialog = document.querySelector('dialog');
 
-addButton.addEventListener('click', () => dialog.showModal());
-closeButton.addEventListener('click', () => dialog.close());
+openFormButton.addEventListener('click', () => dialog.showModal());
+closeFormButton.addEventListener('click', () => dialog.close());
 
-console.log(closeButton)
+const bookForm = document.getElementById('book-form');
+const bookList = document.querySelector('.book-list');
 
-
-function book(author, title, numOfPages, isRead) {
-    this.author = author;
+function Book(title, author, numOfPages, isRead) {
     this.title = title;
+    this.author = author;
     this.numOfPages = numOfPages;
     this.isRead = isRead;
 }
 
 function addBookToLibrary() {
-    
+    const book = new Book();
+    const formData = new FormData(bookForm);
+    for (const [name, value] of formData) {
+        book[name] = value;
+    }
+    myLibrary.push(book);
 }
+
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+});
+
+
