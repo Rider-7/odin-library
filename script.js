ee04e69e8897cdb3c0056c591a313239c7e0c8d0
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-24 16:52:56 +0800

    Add dialog form for new books

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const myLibrary = [];
 
+const addButton = document.querySelector('.add-button');
+const closeButton = document.querySelector('.cancel-button');
+const dialog = document.querySelector('dialog');
+
+addButton.addEventListener('click', () => dialog.showModal());
+closeButton.addEventListener('click', () => dialog.close());
+
+console.log(closeButton)
+
+
 function book(author, title, numOfPages, isRead) {
     this.author = author;
     this.title = title;
